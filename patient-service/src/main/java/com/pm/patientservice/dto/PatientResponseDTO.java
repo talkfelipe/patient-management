947717783ab5f3ea8e508e070d43a0653f57da25
Felipe Silva <talk.felipe@gmail.com>
2026-02-05 21:32:44 +0900

    created classes and methods for creating a patient in the database

--- patient-service/src/main/java/com/pm/patientservice/dto/PatientResponseDTO.java
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientResponseDTO.java
@@ -6,6 +6,7 @@ public class PatientResponseDTO {
     private String email;
     private String address;
     private String dateOfBirth;
+    private String registeredDate;
 
     public String getId() {
         return id;
@@ -44,4 +45,8 @@ public class PatientResponseDTO {
     public void setDateOfBirth(String dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
+
+    public String getRegisteredDate() { return registeredDate; }
+
+    public void setRegisteredDate(String registeredDate) { this.registeredDate = registeredDate; }
 }
