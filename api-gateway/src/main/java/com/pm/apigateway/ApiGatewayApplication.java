4b8c6b02b217c91393acb140b76601777c4dfaba
Felipe Silva <talk.felipe@gmail.com>
2026-02-24 12:47:12 +0900

    api access only with token to the patients service implemented

--- api-gateway/src/main/java/com/pm/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/pm/apigateway/ApiGatewayApplication.java
@@ -2,6 +2,8 @@ package com.pm.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -10,4 +12,8 @@ public class ApiGatewayApplication {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
 }
